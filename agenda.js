ea49ea815e4c142600da94f625e0be32bf83c2c3
anikape <ana.br@hotmail.com>
2022-07-01 11:29:31 -0300

    Alterações na pag agenda

---
+++ agenda.js
@@ -0,0 +1,12 @@
+let select = document.querySelector('#agenda');
+
+let value = select.options[select.selectedIndex].value;
+
+function mudarmes() {
+    let select = document.querySelector('#agenda');
+    let optionvalue = select.options[select.selectedIndex];
+
+    let value = option.value
+}
+
+mudarmes()
